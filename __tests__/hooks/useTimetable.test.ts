4a6f5bd8a3a02e64534e22efb1548ea90f823bc1
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-06-24 17:21:11 +0900

    refactor: DRY timetable variable

--- __tests__/hooks/useTimetable.test.ts
+++ __tests__/hooks/useTimetable.test.ts
@@ -3,12 +3,16 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import useTimetable, { TimetableProps } from '~/hooks/useTimetable';
 
 describe('useTimetable hooks', () => {
-  it('set initial timetable', () => {
-    const timetable = {
+  let timetable: TimetableProps;
+  beforeEach(() => {
+    timetable = {
       schedule: [0, 1, 2, 3, 4, 5, 6],
       // 06:00, 06:30, 07:10, 22:50, 23:30, 00:30
       data: [7200, 9000, 11400, 67800, 70200, 73800],
     };
+  });
+
+  it('set initial timetable', () => {
     const currentTime = new Date('2020-05-28T07:09:30').getTime();
     const { result } = renderHook(() => useTimetable(timetable, currentTime));
     expect(result.current.index).toBe(0);
@@ -16,15 +20,6 @@ describe('useTimetable hooks', () => {
   });
 
   describe('時間経過で保持する時刻表データも進む', () => {
-    let timetable: TimetableProps;
-    beforeEach(() => {
-      timetable = {
-        schedule: [0, 1, 2, 3, 4, 5, 6],
-        // 06:00, 06:30, 07:10, 22:50, 23:30, 00:30
-        data: [7200, 9000, 11400, 67800, 70200, 73800],
-      };
-    });
-
     it('発車時刻ちょうどの場合は残り時間は`0`', () => {
       const currentTime = new Date('2020-05-28T07:10:00').getTime();
       const { result } = renderHook(() => useTimetable(timetable, currentTime));
